d32c45a6e0a498a07c623297d5d85a2c27ae2542
sunnyyuanz <you@example.com>
2022-06-01 23:22:29 -0400

    replace classlist.addclass and removeclass by toggleclass

--- js/site.js
+++ js/site.js
@@ -3,9 +3,5 @@ let menuClick = document.querySelector(".menubar-mobile");
 let menubar = document.querySelector(".menubar");
 
 menuClick.addEventListener('click', function () {
-    if (menubar.classList.contains("clicked")) {
-        menubar.classList.remove("clicked");
-    } else {
-        menubar.classList.add("clicked");
-    }
+    menubar.classList.toggle("clicked")
 })
