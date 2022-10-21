ed7b7a2bbca61cbea591064e2bfbcdfc7fc34115
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-10-21 12:27:26 -0400

    form validation

--- js/site.js
+++ js/site.js
@@ -1,8 +1,55 @@
-let menuClick = document.querySelector(".menubar-mobile");
+let menuClick = document.querySelector('.menubar-mobile');
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let nameErr = document.getElementById('nameErr');
+let emailErr = document.getElementById('emailErr');
 
-let menubar = document.querySelector(".menubar");
+let menubar = document.querySelector('.menubar');
 
 menuClick.addEventListener('click', function () {
-    menubar.classList.toggle("clicked")
-})
+  menubar.classList.toggle('clicked');
+});
 
+const form = document.getElementById('contact');
+
+function validation() {
+  let isReq = true;
+
+  if (!name) {
+    nameErr.innerHTML = 'Please give me a name';
+    isReq = false;
+  }
+  if (!email) {
+    emailErr.innerHTML = 'Please give me an email';
+    isReq = false;
+  } else if (!email.includes('.') || !email.includes('@')) {
+    emailErr.innerHTML = 'Please give me a valid email';
+    isReq = false;
+  }
+
+  return isReq;
+}
+
+// function submitForm(e) {
+//   e.preventDefault();
+//   const isReq = validation();
+//   console.log(isReq);
+
+//   if (isReq) {
+//     alert('Submitted! Will be in touch with you soon!');
+//     console.log('form is submitted!');
+//   }
+//   console.log('submit is clicked');
+// }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const isReq = validation();
+  console.log(isReq);
+
+  if (isReq) {
+    alert('Submitted! Will be in touch with you soon!');
+    console.log('form is submitted!');
+  }
+  console.log('submit is clicked');
+});
