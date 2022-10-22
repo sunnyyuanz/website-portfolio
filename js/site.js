e5834d44a645603a6bda6330745be1ea7625cc6e
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-10-22 16:10:20 -0400

    fixing bug

--- js/site.js
+++ js/site.js
@@ -1,34 +1,19 @@
 let menuClick = document.querySelector('.menubar-mobile');
-let name = document.getElementById('name').value;
-let email = document.getElementById('email').value;
-let nameErr = document.getElementById('nameErr');
-let emailErr = document.getElementById('emailErr');
-
 let menubar = document.querySelector('.menubar');
 
 menuClick.addEventListener('click', function () {
   menubar.classList.toggle('clicked');
 });
 
-const form = document.getElementById('contact');
+// function validation() {
+//   let isReq = true;
+//   if (!email.includes('.') || !email.includes('@')) {
+//     emailErr.innerHTML = 'Please give me a valid email';
+//     isReq = false;
+//   }
 
-function validation() {
-  let isReq = true;
-
-  if (!name) {
-    nameErr.innerHTML = 'Please give me a name';
-    isReq = false;
-  }
-  if (!email) {
-    emailErr.innerHTML = 'Please give me an email';
-    isReq = false;
-  } else if (!email.includes('.') || !email.includes('@')) {
-    emailErr.innerHTML = 'Please give me a valid email';
-    isReq = false;
-  }
-
-  return isReq;
-}
+//   return isReq;
+// }
 
 // function submitForm(e) {
 //   e.preventDefault();
@@ -42,14 +27,18 @@ function validation() {
 //   console.log('submit is clicked');
 // }
 
+const form = document.getElementById('contact');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const isReq = validation();
-  console.log(isReq);
-
-  if (isReq) {
-    alert('Submitted! Will be in touch with you soon!');
-    console.log('form is submitted!');
-  }
-  console.log('submit is clicked');
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
+  let msg = document.getElementById('message').value;
+  alert('Submitted! Will be in touch with you soon!');
+  console.log('form is submitted!');
+  localStorage.setItem('name', name);
+  localStorage.setItem('email', email);
+  localStorage.setItem('phone', phone);
+  localStorage.setItem('msg', msg);
 });
